1874c166c22729c213cef5d0344c8967f04df0db
z1skgr <z1skgr@yahoo.gr>
2022-02-26 15:45:15 +0200

    rec

---
+++ LAB10224373_set1_exer_1.c
@@ -0,0 +1,83 @@
+/*Χρήστος Ζήσκας 2014030191*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int recursive(int *array,int size_of_array,int i); /*Prototipo anadromikis*/
+
+int main()
+{
+	int menu_answer;
+	int flag=0,temp;
+	int count_of_numbers,i,result;				/*Dilosi metavliton*/
+	int *array;								/*Dilosi pinako me simasiologia dikti*/
+	
+	printf("Welcome to the 1st Project!\n\n");
+	
+	while(flag==0)
+	{
+		printf("1.Activate Program\n");
+		printf("2.Exit\n\n");
+		printf("Choose your answer('1' to activate,'2' for exit): ");
+		scanf(" %d", &menu_answer);
+		printf("\n");
+		
+		if(menu_answer==1)
+		{
+			printf("\nEnter the count of numbers do you want to use: ");
+			scanf("%d", &count_of_numbers);		
+	
+			array=(int*)malloc(count_of_numbers*sizeof(int));								/*Desmeusi mnimis gia na xoresoun oloi oi arithmoi ston pinaka*/
+		
+			if(array==0)																/*Elegxos an iparxei diathesimi mnimi sto pc*/
+			{
+				printf("Error! No Memory");
+				exit(1);
+			}
+			
+			for(i=0;i<count_of_numbers;i++)											/*Oi deiktes dixnoun stous arithmous pou pliktrologei o xristis*/
+			{
+					printf("Type the number of your choice: ");
+					scanf("%d", array+i);
+			}
+			
+			result=recursive(array,count_of_numbers,0);
+			printf("\n");									/*to apotelesma ths anadromikhs apothikeuete se metavliti*/
+			if(result==-1)												
+			{
+				printf("Den iparxei zeugos pou na ikanopoiei th sinthiki!\nTimi epistrofis = %d",result); 		/*Apousia katoptrikou zeugous gia timi ths c=-1*/
+				printf("\n\n");	
+			}
+			else
+				printf("\nH thesi tou prwtou stoixeiou tou katoptrikou zeugous einai %d\n\n", result+1); /*Emfanisi th thesis tou prwtou stoixeiou tou katoptrikou zeugous*/	
+			free(array); 			/*Apodesmeusi mnimis*/
+		}
+		
+		
+		if(menu_answer==2)
+		{
+			flag=1;
+			printf("You have just selected the output mode.\nEnjoy your work.\nHave a nice day!\n\n");
+		}
+	}
+	
+
+	
+	system("Pause");
+	
+	return 0;
+}
+
+int recursive(int *array,int size_of_array,int i)								//*Anadromiki Sinartisi*//
+{
+	if(i==size_of_array)										/*An o diktis ftasei sto telos tou megethous tou pinaka simenei oti dn exei vrethei zeugos*/
+		return -1;
+		
+	if(array[i]+array[size_of_array-1-i]==array[0])				/*An ikanopoieite h sinthiki iparxei toulaxiston ena zeugos pou ikanopoiei th sinthiki*/
+	{
+		printf("The numbers %d and %d satisfies the condition", *(array+i),*(array+size_of_array-i-1));	
+		printf("\n");
+		return i;													/*Epistrefete h thesi tou arithmou apo to zeugos pou vriskete prwto*/
+	}
+	recursive(array,size_of_array,i+1); 							/*Xanakaleite h sinartisi*/
+}
---
+++ LAB10224373_set1_exer_2 .c
@@ -0,0 +1,125 @@
+/*Χρήστος Ζήσκας 2014030191*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct list
+{
+	int data;					/*Prototipo listas*/
+	struct list *next;
+}*start=NULL;
+																	/*Prototipa sinartiseon*/
+struct list* recursive(struct list* ptr1);
+
+void freeList(struct list *node);
+
+int main()
+{
+	struct list *current,*new_node,*n1,*result;	
+	int count_of_nodes,i,flag=0,menu_answer;			/*Dilosi metavliton*/
+	n1=result=NULL;/*Arxikopoihsh deikton*/
+	
+	
+	printf("Welcome to the 2nd Project!\n\n");
+	
+	while(flag==0)
+	{
+		printf("1.Activate Program\n");
+		printf("2.Exit\n\n");					/*Menu- 1 gia energopoihsh tou programmatos- 2 gia exodo*/
+		printf("Choose your answer('1' to activate,'2' for exit): ");
+		scanf(" %d", &menu_answer);
+		printf("\n\n");
+		
+		
+		if(menu_answer==1)		/*Gia apantisi 1*/
+		{				
+			printf("\nEnter the count of nodes do you want in your list: ");	/*Diavazete o arithmos ton komvon pou epithimei o xristis*/
+			scanf("%d", &count_of_nodes);
+			printf("\n");
+			i=0;
+			
+			while(i<count_of_nodes)
+			{
+				new_node=(struct list*)malloc(sizeof(struct list));		/*Desmeusi mnimis pou apaiteite gia ton pointer oste na apothikeuei ta dedomena*/
+				
+				printf("\nEnter the data of the node(%d) :",i);
+				scanf("%d", &new_node->data);					/*Ekxorisi timis sth metavliti data */
+				new_node->next=NULL;				/*O deiktis new_node ton ekxoroume na min dixnei pouthena afou dn iparxei kati gia na deixei*/
+				
+				
+				
+				if(start==NULL)									/*o deiktis start prepei na dixnei sth prwth thesi ths lista*/
+				{
+					start=new_node;									/*An den dixnei pouthena prepei na dixnei sth prwth thesi p exei dimiourgithi diladi sth thesi tou new_node*/
+					n1=new_node;										
+					current=new_node;
+				}
+			
+				
+				else
+				{
+					current->next=new_node;		/*Voithitiki metavliti gia na sindeuete kathe komvos me ton epomeno*/
+					current=new_node;
+				}
+				i++;
+					
+			}
+			if(count_of_nodes<=2)				/*An iparxoun ligoteroi apo 2 komvoi tote theoroume oti ikanopoihte to kritirio*/
+				printf("Your list satisfies the condition!\n\n");
+			else
+				result=recursive(n1);				/*An iparxoun parapano tote energopoihte h sinartisi recursive*/
+			
+			if(result==NULL)									/*Gia result=null simeni oti h lista ikanopoiei to kritirio*/
+				printf("Timi epistrofis tis sinartishs: %s\n\n",result);
+			else
+				printf("Timi epistrofis tis sinartishs %d kai %p\n\n",result->data,result);
+			free(new_node);/*Apodesmeusi mnimis*/
+			start=NULL;		
+			
+			
+		}
+		if(menu_answer==2)	/*Gia apantisi tou xristi 2 tote to programma termatizete*/
+		{
+			flag=1;
+			printf("\nYou have just selected the output mode.\nEnjoy your work.\nHave a nice day!\n\n");
+		}
+	
+	}
+	return 0;
+}
+
+struct list* recursive(struct list* ptr1)	/*Sinartisi gia elegxo tis lista an ikanopoiei ti zhtoumenh sxesi*/
+{
+       struct list *s;
+       s=NULL;
+       
+       
+       if(ptr1->next->next==NULL)			/*An isxuei h sxesi kai ginei null tote shmenei oti ftasame sto telos ths listas kai dn exei vrethei kapoia timh*/
+       {									/*sth lista pou na mn ikanopoiei th sxesi*/
+           printf("\nYour list satisfies the condition!\n\n\n");
+           return NULL;
+		}
+       
+       if((ptr1->data)<(ptr1->next->data*ptr1->next->next->data))		/*h zitoumenh sxesi*/
+       {
+        	recursive(ptr1->next);	/*Anadromiki klisi ths sinartisis*/
+        }
+       else
+       {	
+           s=ptr1;					/*An dn ikanopoieite h sxesi tote epistrefete deiktis pou dixnei ston prwto arithmo*/
+           printf("\nYour list doesnt satisfies the condition!");			/*apo thn triada pou dn ikanopoiei th sinthiki*/
+           printf("\nThe first number of the 3 consecutive numbers %d %d %d is %d", ptr1->data,ptr1->next->data,ptr1->next->next->data,s->data);
+           printf("\n\n\n");
+           return s;
+    }
+}
+
+void freeList(struct list *node){		/*Sinartisi apodesmeusi mnimis tis listas*/
+	struct list *tmp = node;			/*Xrisimopoieite voithitiki metavliti gia na mn xanonte oi proigoumenoi komvoi*/
+    
+    while(tmp != NULL){
+		node = node->next;
+		free(tmp);
+		tmp = node;
+    }
+}
